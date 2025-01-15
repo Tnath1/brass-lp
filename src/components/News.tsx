44cd5decd2686a06c7ad714bab085a2f2fa9a1d8
Tnath1 <aromejonathanu@gmail.com>
2025-01-15 20:04:13 +0100

    hero 50% ready

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,8 +1,18 @@
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const News = () => {
   return (
-    <h1 className="">News</h1>
-  )
-}
+    <div className=" w-full bg-[#1a1c1f] h-[40px] flex justify-center items-center">
+      <a href="" className="flex items-center justify-center  text-[#fff]">
+        <div className=" px-[9px] py-[5px] text-[10px] mr-[8px] text-white font-[500] bg-[#0bce5a] rounded-[12px] ">NEW</div>
+        <p className="text-[13px] text-[#fff] ">
+          Introducing Brass Payroll: The Ultimate Payroll Solution for Your
+          Business
+        </p>
+        <  IoIosArrowRoundForward className="ml-[12px] text-[20px] "/>
+      </a>
+    </div>
+  );
+};
 
-export default News
+export default News;
